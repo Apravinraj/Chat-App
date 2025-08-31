2b7863c945eaee4047d804510248aa7d61927811
Apravinraj <pravinraj2k5@gmail.com>
2025-08-31 23:50:20 +0530

    final

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -12,7 +12,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("pravin-chat-app.up.railway.app")
+                .setAllowedOrigins(
+                        "http://localhost:8080",
+                        "https://pravin-chat-app.up.railway.app"
+                )
                 .withSockJS();
     }
 
